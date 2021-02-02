ebbe18d292153faa7ea98adeb8bc81dbf4a854bf
Joey <786678257@qq.com>
2021-02-02 20:56:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="max-w-sm mx-auto text-center mt-10">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+  },
+  setup() {
+    return {
+    };
+  },
+});
+</script>
